75ee18b9f70bdda068fa181bdb94e916b214b4e3
ChrisNalle <adickevin02@gmail.com>
2022-01-09 17:04:17 +0700

    Merubah index menggunakan tabel

--- index.php
+++ index.php
@@ -11,7 +11,19 @@ $file = "daftar.json";
 
 $datanime = file_get_contents($file, false, stream_context_create($arrContextOptions));
 $data = json_decode($datanime, true);
-
-echo $data[0]["kode_nime"];
-echo $data[0]["nama_nime"];
 ?>
+<table border="1">
+	<tr>
+		<td>Kode</td>
+		<td>Nama Anime</td>
+		<td>Tahun Anime</td>
+		<td>Musim Anime</td>
+		<td>Total Episode</td>
+	<tr>
+		<td><?php echo $data[0]["kode_nime"];?></td>
+		<td><?php echo $data[0]["nama_nime"];?></td>
+		<td><?php echo $data[0]["tahun_nime"];?></td>
+		<td><?php echo $data[0]["musim_nime"];?></td>
+		<td><?php echo $data[0]["total_eps"];?></td>
+	</tr>
+</table>
