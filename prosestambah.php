8cc5da3ec05d56503260a38e33cb3d862a124a86
ChrisNalle <adickevin02@gmail.com>
2022-01-09 18:00:23 +0700

    Membuat proses tambah

---
+++ prosestambah.php
@@ -0,0 +1,11 @@
+<?php 
+    include 'koneksi.php';
+    $kode_nime = $_POST['kode_nime'];
+    $nama_nime = $_POST['nama_nime'];
+    $tahun_nime = $_POST['tahun_nime'];
+    $musim_nime = $_POST['musim_nime'];
+    $total_eps = $_POST['total_eps'];
+
+    mysqli_query($koneksi,"insert into daftarnime values('$kode_nime','$nama_nime','$tahun_nime','$musim_nime','$total_eps')");
+    header("location:daftarnime.php");
+?>
